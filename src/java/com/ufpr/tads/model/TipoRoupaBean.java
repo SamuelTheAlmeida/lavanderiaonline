ca07a81fbb4bcfa3dc51254b2f83a7cef1904f12
Gustavo <gustavokovalski.saporiti@gmail.com>
2019-05-23 16:43:54 -0300

    adicionado atributos na model

--- src/java/com/ufpr/tads/model/TipoRoupaBean.java
+++ src/java/com/ufpr/tads/model/TipoRoupaBean.java
@@ -5,7 +5,50 @@
  */
 package com.ufpr.tads.model;
 
+import java.io.Serializable;
 
-class TipoRoupaBean {
+
+public class TipoRoupaBean implements Serializable {
     
+   private int idTipoRoupa;
+   private double preco;
+   private String descricao;
+   private int prazoLavagem;
+   
+
+    public TipoRoupaBean() {
+    }
+   
+    public int getIdTipoRoupa() {
+        return idTipoRoupa;
+    }
+
+    public void setIdTipoRoupa(int idTipoRoupa) {
+        this.idTipoRoupa = idTipoRoupa;
+    }
+    
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+    
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
+      public int getPrazoLavagem() {
+        return prazoLavagem;
+    }
+
+    public void setPrazoLavagem(int prazoLavagem) {
+        this.prazoLavagem = prazoLavagem;
+    }
+ 
 }
